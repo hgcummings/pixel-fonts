477c92e0acfb5f74bc6b377569cba31b645eb840
hgcummings <harry@xlr3.com>
2018-06-16 14:29:08 +0100

    Update package name

--- samples.js
+++ samples.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 
 Object.entries(fonts).forEach(([name, font]) => {
     renderImage(
-        `Name: ${font.name}\n \nABCDEFGHIJKLMNOPQRSTUVWXYZ!\nabcdefghijklmnopqrstuvwxyz.`,
+        `Font name: ${font.name}\n \nABCDEFGHIJKLMNOPQRSTUVWXYZ!\nabcdefghijklmnopqrstuvwxyz.`,
         font,
         {
             foreground: [0, 0, 0, 255],
             background: [255, 255, 255],
-            scale: 3
+            scale: 2
         }
     ).pipe(fs.createWriteStream(`./samples/${name}.png`));
 });
